fc6a3e46b40faabfd1af583159b069dc3f536779
ivoosantos <oliveiradossantosivo@gmail.com>
2020-04-18 00:08:09 -0300

    Registrar PredictionEnginePool para uso no aplicativo

---
+++ Controllers/PredictController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.ML;
+using TensorFlowWebAPI.DataModels;
+
+namespace TensorFlowWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredictController : ControllerBase
+    {
+        private readonly PredictionEnginePool<SentimentData, SentimentPrediction> _predictionEnginePool;
+        public PredictController(PredictionEnginePool<SentimentData, SentimentPrediction> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] SentimentData input)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            SentimentPrediction prediction = _predictionEnginePool.Predict(modelName: "TensorFlowModel", example: input);
+
+            string sentiment = Convert.ToBoolean(prediction.Prediction) ? "Positivo" : "Negativo";
+
+            return Ok(sentiment);
+        }
+    }
+}
