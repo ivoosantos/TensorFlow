4b5b6489e2887bd7f43b43af559efbd60c5d41dd
ivoosantos <oliveiradossantosivo@gmail.com>
2020-04-17 23:47:26 -0300

    Iniciando o projeto de Análise de Sentimento

---
+++ DataModels/SentimentPrediction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace TensorFlowWebAPI.DataModels
+{
+    public class SentimentPrediction : SentimentData
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+        public float Probability { get; set; }
+        public float Score { get; set; }
+    }
+}
